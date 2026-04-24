fec78ed21b50bfd56fb5ab7c0eb2d8000e5101a6
Khushi Verma <khushi.jm.verma@gmail.com>
2026-04-24 15:13:18 -0500

    Remove frontend/.next cache files from git tracking

--- frontend/.next/dev/server/chunks/ssr/frontend__next-internal_server_app_assignments_[id]_page_actions_0y6hoyq.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/frontend/.next-internal/server/app/assignments/[id]/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=frontend__next-internal_server_app_assignments_%5Bid%5D_page_actions_0y6hoyq.js.map
--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HXct8omeR2SIVZosyv+9E7fdYqcX8kQgU9WvxvggqOk=\"\n}"
